b791d36ded3e06577bbb66ac0f13595f1d2c240d
Manahal Tabassum <mtabassum@stuy.edu>
2017-05-28 21:29:30 -0400

    Worked on trie

--- Trie.java
+++ Trie.java
@@ -40,6 +40,60 @@ public class Trie{
 	current.setIsWord(true);
     }
 
+    public boolean containsPrefix(String prefix){
+	char[] prefixLetters = new char[prefix.length()];
+	for (int x = 0; x < prefix.length(); x++){
+	    prefixLetters[x] = prefix.charAt(x);
+	}
+	return contains(prefixLetters, false);
+    }
+
+    public boolean containsWord(String word){
+	char[] letters = new char[word.length()];
+	for (int x = 0; x < word.length(); x++){
+	    letters[x] = word.charAt(x);
+	}
+	return contains(letters, true);
+    }
+
+    public Node getWord(String word){
+	char[] letters = new char[word.length()];
+	for (int x = 0; x < word.length(); x++){
+	    letters[x] = word.charAt(x);
+	}
+	Node node = getNode(letters);
+	if (node != null && node.isWord()){
+	    return node;
+	}
+	return null;
+    }
+
+    public Node getPrefix(String prefix){
+	char[] letters = new char[prefix.length()];
+	for (int x = 0; x < prefix.length(); x++){
+	    letters[x] = prefix.charAt(x);
+	}
+	return getNode(letters);
+    }
+
+    private boolean contains(char[] word, boolean isWord){
+	Node node = getNode(word);
+	return (node != null && node.isWord() && isWord) ||
+	    (!isWord && node != null);
+    }
+
+    private Node getNode(char[] word){
+	Node current = root;
+
+	for (int x = 0; x < word.length && current != null; x++){
+	    current = current.getChild(word[x]);
+	    if (current == null){
+		return null;
+	    }
+	}
+	return current;
+    }
+
     public static void main (String args[]){
     }
 }
